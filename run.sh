b697801057e20b7e549ffec7a5e6502fad13cf20
b00ris <b00ris@mail.ru>
2019-06-15 19:20:19 +0300

    add second account

--- run.sh
+++ run.sh
@@ -7,8 +7,10 @@ make install
 hhd init node_name --chain-id hhchain
 
 hhcli keys add validator1
+hhcli keys add user
 
 hhd add-genesis-account $(hhcli keys show validator1 -a) 1000hhtoken,100000000stake
+hhd add-genesis-account $(hhcli keys show user -a) 1000hhtoken
 
 hhcli config chain-id hhchain
 hhcli config output json
