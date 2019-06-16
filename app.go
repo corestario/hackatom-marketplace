e3484f4e45ace2cb97889b0a4575fc262ba2dad4
b00ris <b00ris@mail.ru>
2019-06-16 18:12:45 +0300

    fix amino

--- app.go
+++ app.go
@@ -61,8 +61,6 @@ func init() {
 // MakeCodec generates the necessary codecs for Amino
 func MakeCodec() *codec.Codec {
 	var cdc = codec.New()
-	auth.RegisterCodec(cdc)
-	bank.RegisterCodec(cdc)
 	ibck.RegisterCodec(cdc)
 	ModuleBasics.RegisterCodec(cdc)
 	sdk.RegisterCodec(cdc)
